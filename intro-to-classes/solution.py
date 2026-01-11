1d63f16897b14ea86c8b0a7c84845520fafa7f8f
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-11 14:38:52 +0530

    Add solution for Intro to Classes

---
+++ intro-to-classes/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: Intro to Classes
+URL: https://neetcode.io/problems/python-intro-to-classes/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class Pet:class Pet:
+    def __init__(self,name:str,species:str):    def __init__(self,name:str,species:str):
+        self.name=name        self.name=name
+        self.species=species        self.species=species
+
+
+
+
+# Do not modify below this line# Do not modify below this line
+my_pet = Pet("Fluffy", "cat")my_pet = Pet("Fluffy", "cat")
+print(f"My pet is a {my_pet.species} named {my_pet.name}")print(f"My pet is a {my_pet.species} named {my_pet.name}")
